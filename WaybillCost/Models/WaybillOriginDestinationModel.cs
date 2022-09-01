2eb72223da7cd39abb9efd8db3c49a124f118136
david flores <dcfloresjr@hotmail.com>
2022-09-02 03:00:28 +0800

    fix footer copyright

---
+++ WaybillCost/Models/WaybillOriginDestinationModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WaybillCost.Models
+{
+    public class WaybillOriginDestinationModel
+    {
+        public WayBill waybillDetails { get; set; }
+        public Origin originDetails { get; set; }
+        public Destination destinationDetails { get; set; }
+    }
+}
